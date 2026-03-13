2aba1b74054a12bd3221763eeab96c04fb506f29
bakernigel <bakernigel@yahoo.com>
2026-03-13 10:42:01 -0700

    Remove bad copyright lines

--- custom_components/apsmeter/const.py
+++ custom_components/apsmeter/const.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2021-2023 Luis López <luis@cuarentaydos.com>
-#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
